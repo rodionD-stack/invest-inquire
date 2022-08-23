c602e734a63bb77650aebef99425afa0a2e256e8
Rodion Dudar <rodion.dudar.rd@gmail.com>
2022-08-23 12:38:24 +0300

    *ININQ-5//Обновлен параметр отображения данных диаграмм.

--- src/Components/Home/StocksPortfolio/StockDiagramItem.js
+++ src/Components/Home/StocksPortfolio/StockDiagramItem.js
@@ -1,13 +1,26 @@
 import React from 'react';
 import { Doughnut } from 'react-chartjs-2';
-import { Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js";
+import { Chart as ChartJS, ArcElement, Tooltip, Legend} from "chart.js";
 
 ChartJS.register(ArcElement, Tooltip, Legend)
+
+const options = {
+    plugins: {
+        tooltip: {
+            callbacks: {
+                label: (value) => {
+                    console.log(value)
+                    return value.label + ':' + ' ' + value.formattedValue + '%'
+                }
+            }
+        },
+    },
+}
 
 export const StockDiagramItem = ({param}) => {
     return (
         <div style={{width: '300px'}}>
-            <Doughnut data={param}/>
+            <Doughnut options={options} data={param}/>
         </div>
         
     )
